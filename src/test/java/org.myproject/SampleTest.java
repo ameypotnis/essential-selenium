dbb824bb75b28733c7af9c04dfaf4a6f4c038b84
Amey <apotnis@equalexperts.com>
2016-07-02 10:04:43 +0530

    adding sample test

---
+++ src/test/java/org.myproject/SampleTest.java
@@ -0,0 +1,32 @@
+package org.myproject;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.jcabi.aspects.Cacheable;
+import com.jcabi.jdbc.JdbcSession;
+import com.jcabi.jdbc.SingleOutcome;
+import com.jolbox.bonecp.BoneCPDataSource;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.openqa.selenium.By;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+/**
+ * Created by amey on 2/7/16.
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class SampleTest {
+    @Test
+    public void should() {
+        //given
+        //when
+        //then
+    }
+}
